0d58bc0c3360f787bfdd45290ffdbb0b1b448ec1
David van Oudheusden <d.vanoudheusden@rocva.nl>
2019-11-28 18:28:17 +0100

    different approach to game

--- js/script.js
+++ js/script.js
@@ -1,87 +1,45 @@
-var getalOud;
+var getalOud = Math.floor((Math.random() * 100) + 1);
 var getalNieuw;
-var score = 0;
-//hou bij hoevaak iemand een beurt heeft gehad.
-var aantalKeer = 0;
+var aantalXfoutief = 0;
+var aantalXGoed = 0;
+var score;
 
-//hier word het scorebord gevuld met lege scores
-for (i = 0; i < 10; i++) {
-    document.getElementById("scoreboard").innerHTML += 0 + "<br>";
-}
-//hier wordt het scorebord gevuld met "lege" namen
-for (i = 0; i < 10; i++) {
-    document.getElementById("scoreboard").innerHTML += + "<br>";
-}
+document.getElementById("numberOnSCreen").innerHTML = getalOud;
 
+function hogerAntwoord() {
 
-function init() {
-    //genereer een random getal tussen en 1 en de 100
-    getalOud = ;
-    //toon dit getal op het scherm
+    getalNieuw = Math.floor((Math.random() * 100) + 1);
+    document.getElementById("numberOnSCreen").innerHTML = getalNieuw;
 
-    //disable alle buttons behalve de start button en de spluitleg button
+    if (getalNieuw > getalOud) {
+        aantalXGoed = aantalXGoed + 1;
+        document.getElementById("aantalGoed").innerHTML = aantalXGoed;
 
-    //toon het scorebord
-    toonScoreBord();
-}
-
-function start() {
-    // genereer een nieuw getal. gebruik daar de functie genereerGetal voor
-
-    //enable de buttons hoger, lager en einde
-
-}
-function genereerGetal() {
-    //geneneer een nieuwe random getal tussen de 1 en de 100
-    getalNieuw = ;
-    // dit getal moet anders zijn dan het vorige getal. gebruik een while loop om dat voor elkaar te krijgen
-
-
-    //toon het getal op het scherm
-    document.getElementById("getal").innerHTML = ;
-
-}
-
-function vergelijk(keuze) {
-    //afhankelijk van de keuze die gemaakt is wordt gecontroleerd of deze keuze correct is
-
-
-    //toon de score op het scherm
-
-    //bewaar het huidige getal in getalOud
-
-    //houdt bij hoevaak iemand geweest is
-
-    // is dit 10 keer , stop dan het spel, genereer anders een nieuw getal
-
-}
-
-function einde() {
-    //vraag om de naaam van de speler
-    var naam =;
-    //maak score bord
-
-    // zet de score op de juiste plek. gebruik hiervoor splice n pop
-    for (i = 0; i < scorebord.length; i++) {
-        if (score > scorebord[i]) {
-
-            break;
-        }
+    } else {
+        aantalXfoutief = aantalXfoutief + 1;
+        document.getElementById("aantalFout").innerHTML = aantalXfoutief;
     }
-    //reset de waardes (denk ook aan het disablen van diverse buttons)
-
+    score = aantalXGoed * 100 - 25 * aantalXfoutief;
+    document.getElementById("score").innerHTML = score;
+    getalOud = getalNieuw;
 }
-function toonScoreBord() {
-    //toon het scorebord op het scherm
 
-    for (i = 0; i < scorebord.length; i++) {
+function lagerAntwoord() {
 
+    getalNieuw = Math.floor((Math.random() * 100) + 1);
+    document.getElementById("numberOnSCreen").innerHTML = getalNieuw;
+
+    if (getalNieuw < getalOud) {
+        aantalXGoed = aantalXGoed + 1;
+        document.getElementById("aantalGoed").innerHTML = aantalXGoed;
+    } else {
+        console.log("niet goed");
+        aantalXfoutief = aantalXfoutief + 1;
+        document.getElementById("aantalFout").innerHTML = aantalXfoutief;
     }
-}
-function spelUitleg() {
-    //verberg het spel en toon de speluitleg
-}
 
-function sluitUitleg() {
-    //verberg de speluitleg en toon het spel 
+    score = aantalXGoed * 100 - 25 * aantalXfoutief;
+    document.getElementById("score").innerHTML = score;
+    getalOud = getalNieuw;
+
 }
